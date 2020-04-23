4400c7fa5e3357d92e7f6152016f1b0dd9b2c653
tang1908 <393714523@qq.com>
2020-04-23 22:57:30 +0800

    天涯明月刀

--- js/recruit.js
+++ js/recruit.js
@@ -7,4 +7,19 @@ $(function () {
     $('.dialog').hide();
     $('#_overlay_').hide();
   })
+
+
+  var mySwiper = new Swiper('.swiper-container', {
+    autoplay: 5000,//可选选项，自动滑动
+    loop: true,//可选选项，开启循环
+    navigation: {
+      nextEl: '.arrow-right',
+      prevEl: '.arrow-left',
+    },
+    pagination: {
+      el: '.pagination',
+    },
+  })
+
+  
 })
